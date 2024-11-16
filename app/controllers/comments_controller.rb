f06ac9012ed37b6da0ce38a6cde8e943f34e96bc
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-11-16 11:40:00 +0000

    feat: Add a destroy method and action callbacks

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,29 @@
 # Comments Controller
 class CommentsController < ApplicationController
   before_action :set_article
+  before_action :set_comment, only: %i[destroy]
 
   def create
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
   private
 
     def comment_params
-      params.expect(comment: [ :commenter, :body ])
+      params.expect(comment: [ :commenter, :body, :status ])
     end
 
     def set_article
       @article = Article.find(params[:article_id])
+    end
+
+    def set_comment
+      @comment = @article.comments.find(params[:id])
     end
 end
