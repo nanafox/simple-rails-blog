9f771bf606518633b0f7ea7e0a296a225eaddc57
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-11-24 16:21:23 +0000

    chore: Make "public" as the default value for the `status` field

--- db/migrate/20241116105928_add_status_to_articles.rb
+++ db/migrate/20241116105928_add_status_to_articles.rb
@@ -1,5 +1,5 @@
 class AddStatusToArticles < ActiveRecord::Migration[8.0]
   def change
-    add_column :articles, :status, :string
+    add_column :articles, :status, :string, default: "public"
   end
 end
--- db/migrate/20241116105929_add_status_to_comments.rb
+++ db/migrate/20241116105929_add_status_to_comments.rb
@@ -1,5 +1,5 @@
 class AddStatusToComments < ActiveRecord::Migration[8.0]
   def change
-    add_column :comments, :status, :string
+    add_column :comments, :status, :string, default: "public"
   end
 end
