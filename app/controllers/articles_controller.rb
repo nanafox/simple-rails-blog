66bd4efc6cc574d79dc6419fde0858aa86e62b92
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-11-30 16:09:25 +0000

    feat: Add option to filter articles by title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
 
   def index
-    @articles = Article.where(deleted: false)
+    @articles = Article.where(deleted: false).by_query(params[:query])
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,10 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  scope :by_query, ->(query) {
+          where("title LIKE ?", "%#{query}%") if query.present?
+        }
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
